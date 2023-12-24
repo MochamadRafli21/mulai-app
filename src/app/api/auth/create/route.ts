d0a904909d236f0b408257b3e0fce0c823417727
rafli <rafliforbusiness@gmail.com>
2023-12-24 23:35:07 +0700

    Mochamad Rafli | fix seed prisma

--- src/app/api/auth/create/route.ts
+++ src/app/api/auth/create/route.ts
@@ -1,21 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import { validatePassword, generateToken } from "@/libs/auth";
-import { getUserByEmail } from "@/libs/prisma/user"
+import { getUserByEmail, getAllUser } from "@/libs/prisma/user"
 import { loginParser } from '@/libs/parser/auth/login';
 
 
 export async function POST(request: NextRequest) {
   const res = await request.json()
-  const parsedPayload = loginParser(res)
-  const user = await getUserByEmail(parsedPayload.email)
-  if (!user) {
-    throw new Error('User not found')
-  }
   try {
-    await validatePassword(user.password, parsedPayload.password)
-    const token = generateToken(user.email)
-    return NextResponse.json({ data: token });
+    const parsedPayload = loginParser(res)
+    const users = await getAllUser()
+    console.log(users)
+    const user = await getUserByEmail(parsedPayload.email)
+    console.log(user)
+    if (!user) {
+      throw new Error('User not found')
+    }
+    try {
+      await validatePassword(user.password, parsedPayload.password)
+      const token = generateToken(user.email)
+      return NextResponse.json({ data: token });
+    } catch (error) {
+      return NextResponse.json({ error: 'Unauthorized, Please Check your email and password' }, { status: 401 });
+    }
   } catch (error) {
     return NextResponse.json({ error: 'Unauthorized, Please Check your email and password' }, { status: 401 });
   }
--- src/libs/prisma/user/index.ts
+++ src/libs/prisma/user/index.ts
@@ -1,3 +1,12 @@
 export function getUserByEmail(email: string) {
-  return prisma.user.findUnique({ where: { email } })
+  return prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+}
+
+
+export function getAllUser() {
+  return prisma.user.findMany({})
 }
